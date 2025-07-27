999f2fd8875fb538715ec5892d34c9f35fdfeb89
lupusbytes <bertram@lupusbytes.com>
2025-07-27 15:03:42 +0400

    chore: release version 1.3.0

--- src/LupusBytes.Aspire.Hosting.Azure.EventHubs.LiveExplorer/ImageConstants.cs
+++ src/LupusBytes.Aspire.Hosting.Azure.EventHubs.LiveExplorer/ImageConstants.cs
@@ -6,5 +6,5 @@ internal static class ImageConstants
 
     public const string Image = "lupusbytes/event-hub-live-explorer";
 
-    public const string Tag = "1.2.0"; // x-release-please-version
+    public const string Tag = "1.3.0"; // x-release-please-version
 }
