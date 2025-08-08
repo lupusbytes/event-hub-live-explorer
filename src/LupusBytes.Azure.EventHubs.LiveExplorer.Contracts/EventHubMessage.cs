6cd31a037f6e964c131f510ea55585ab18e20f97
Bertram Baltzer Andersen <bertram@lupusbytes.com>
2025-08-08 17:53:23 +0400

    feat: add enqueued time to table

--- src/LupusBytes.Azure.EventHubs.LiveExplorer.Contracts/EventHubMessage.cs
+++ src/LupusBytes.Azure.EventHubs.LiveExplorer.Contracts/EventHubMessage.cs
@@ -3,4 +3,5 @@ namespace LupusBytes.Azure.EventHubs.LiveExplorer.Contracts;
 public record EventHubMessage(
     string PartitionId,
     long SequenceNumber,
+    DateTimeOffset EnqueuedTime,
     string Message);
