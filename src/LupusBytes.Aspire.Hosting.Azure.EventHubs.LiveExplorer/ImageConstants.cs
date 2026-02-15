489b80fc6298efe046e0bc7537b5d1ea8ee8ba43
Bertram Baltzer Andersen <bertram@lupusbytes.com>
2026-02-16 02:24:04 +0400

    chore: release version 2.0.1

--- src/LupusBytes.Aspire.Hosting.Azure.EventHubs.LiveExplorer/ImageConstants.cs
+++ src/LupusBytes.Aspire.Hosting.Azure.EventHubs.LiveExplorer/ImageConstants.cs
@@ -6,5 +6,5 @@ internal static class ImageConstants
 
     public const string Image = "lupusbytes/event-hub-live-explorer";
 
-    public const string Tag = "2.0.0"; // x-release-please-version
+    public const string Tag = "2.0.1"; // x-release-please-version
 }
