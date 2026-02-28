21c8d9ebccd3b80a40b8678183d6faddb4e322e7
Per Kops <per_kops@hotmail.com>
2026-02-28 23:01:56 +0100

    feat(client): add syntax-highlighted JSON in message detail dialog
    - Render JSON tokens with color-coded spans (keys, strings, numbers, booleans, null)
    - Use VS Code-inspired color scheme for dark mode readability

--- src/LupusBytes.Azure.EventHubs.LiveExplorer.Client/Pages/MessageDetailDialog.razor.cs
+++ src/LupusBytes.Azure.EventHubs.LiveExplorer.Client/Pages/MessageDetailDialog.razor.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
@@ -30,6 +32,8 @@ public sealed partial class MessageDetailDialog : ComponentBase
 
     private string FormattedMessage => FormatJson(Message);
 
+    private MarkupString HighlightedMessage => new(HighlightJson(Message));
+
     private static string FormatJson(string raw)
     {
         try
@@ -41,6 +45,90 @@ public sealed partial class MessageDetailDialog : ComponentBase
         {
             return raw;
         }
+    }
+
+    private static string HighlightJson(string raw)
+    {
+        try
+        {
+            using var doc = JsonDocument.Parse(raw);
+            var sb = new StringBuilder();
+            HighlightElement(doc.RootElement, sb, 0);
+            return sb.ToString();
+        }
+        catch (JsonException)
+        {
+            return WebUtility.HtmlEncode(raw);
+        }
+    }
+
+    private static void HighlightElement(JsonElement element, StringBuilder sb, int indent)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.Object:
+                HighlightObject(element, sb, indent);
+                break;
+            case JsonValueKind.Array:
+                HighlightArray(element, sb, indent);
+                break;
+            case JsonValueKind.String:
+                sb.Append("<span class=\"json-string\">\"").Append(WebUtility.HtmlEncode(element.GetString())).Append("\"</span>");
+                break;
+            case JsonValueKind.Number:
+                sb.Append("<span class=\"json-number\">").Append(element.GetRawText()).Append("</span>");
+                break;
+            case JsonValueKind.True:
+            case JsonValueKind.False:
+                sb.Append("<span class=\"json-boolean\">").Append(element.GetRawText()).Append("</span>");
+                break;
+            case JsonValueKind.Null:
+                sb.Append("<span class=\"json-null\">null</span>");
+                break;
+        }
+    }
+
+    private static void HighlightObject(JsonElement element, StringBuilder sb, int indent)
+    {
+        var pad = new string(' ', indent * 2);
+        sb.Append("<span class=\"json-brace\">{</span>\n");
+        var props = element.EnumerateObject().ToList();
+        for (var i = 0; i < props.Count; i++)
+        {
+            var prop = props[i];
+            sb.Append(pad).Append("  ");
+            sb.Append("<span class=\"json-key\">\"").Append(WebUtility.HtmlEncode(prop.Name)).Append("\"</span>");
+            sb.Append("<span class=\"json-colon\">: </span>");
+            HighlightElement(prop.Value, sb, indent + 1);
+            if (i < props.Count - 1)
+            {
+                sb.Append(',');
+            }
+
+            sb.Append('\n');
+        }
+
+        sb.Append(pad).Append("<span class=\"json-brace\">}</span>");
+    }
+
+    private static void HighlightArray(JsonElement element, StringBuilder sb, int indent)
+    {
+        var pad = new string(' ', indent * 2);
+        sb.Append("<span class=\"json-bracket\">[</span>\n");
+        var items = element.EnumerateArray().ToList();
+        for (var i = 0; i < items.Count; i++)
+        {
+            sb.Append(pad).Append("  ");
+            HighlightElement(items[i], sb, indent + 1);
+            if (i < items.Count - 1)
+            {
+                sb.Append(',');
+            }
+
+            sb.Append('\n');
+        }
+
+        sb.Append(pad).Append("<span class=\"json-bracket\">]</span>");
     }
 
     private async Task CopyFormattedAsync()
